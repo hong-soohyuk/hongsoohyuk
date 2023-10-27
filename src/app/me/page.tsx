f7c144ce2dd795b335cc5cfc09984c845574a846
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2023-10-28 01:03:29 +0900

    initial commit

---
+++ src/app/me/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Me = () => {
+	return (
+		<div>
+			<h1>Me</h1>
+			<Link href="/">go back to main</Link>
+		</div>
+	)
+}
+
+export default Me
